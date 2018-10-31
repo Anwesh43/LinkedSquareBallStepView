c22d7ac007b4d63e7ebb2fbc05d6a828012f0746
Anwesh43 <anweshthecool0@gmail.com>
2018-10-31 09:13:30 +0530

    adding libraries and defining nodes

--- squareballstepview/src/main/java/com/anwesh/uiprojects/squareballstepview/SquareBallStepsView.kt
+++ squareballstepview/src/main/java/com/anwesh/uiprojects/squareballstepview/SquareBallStepsView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.squareballstepview
 /**
  * Created by anweshmishra on 31/10/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+
+val sides : Int = 4
